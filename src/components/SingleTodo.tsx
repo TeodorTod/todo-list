8ade33abc5975717ff72d1dfddcab9786a562c4e
Artos Web <teodor.at.tod@gmail.com>
2022-09-07 15:36:03 +0300

    add useRef hook to edit btn

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdOutlineDone } from "react-icons/md";
@@ -33,18 +33,25 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
         e.preventDefault();
 
         setTodos(todos.map(todo => (
-            todo.id===id
-            ? {...todo, todo: editTodo}
-            : todo
+            todo.id === id
+                ? { ...todo, todo: editTodo }
+                : todo
         )))
         setEdit(false);
     };
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [edit]);
+
 
     return (
         <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
             {
                 edit
                     ? <input
+                        ref={inputRef}
                         value={editTodo}
                         type="text"
                         className="todos__single--text"
